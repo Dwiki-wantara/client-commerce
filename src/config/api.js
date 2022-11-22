5858e1a8aa84874b129a31bbd441e6bbc66dbd63
Dwiki-wantara <tarawantara17@gmail.com>
2022-11-22 10:36:33 +0700

    test

--- src/config/api.js
+++ src/config/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 // buat base url untuk deploy dengan netify
-// export const API = axios.create({
-//   baseURL: process.env.REACT_APP_BASEURL,
-// });
+export const API = axios.create({
+  baseURL: process.env.REACT_APP_BASEURL,
+});
 
 // buat base url untuk local
-export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
-});
+// export const API = axios.create({
+//   baseURL: "http://localhost:5000/api/v1/",
+// });
 
 // Set Authorization Token Header
 export const setAuthToken = (token) => {
